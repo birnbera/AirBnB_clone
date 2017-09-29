a943b2e7f371cc987cf4e4d4e5f68d6a0a81982e
jvpupcat <vuong.joann@yahoo.com>
2017-09-29 23:01:04 +0000

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class BaseModel"""
+
+import json
+import uuid
+from datetime import datetime
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        for k, v in kwargs.items():
+            if k in ['created_at', 'updated_at']:
+                v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+            if k == "__class__":
+                continue
+            setattr(self, k, v)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+		self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+       	d = {}
+        d.update(self.__dict__)
+        d['created_at'] = d['created_at'].isoformat()
+        d['updated_at'] = d['updated_at'].isoformat()
+        d['__class__'] = self.__class__.__name__
+
+        return d
