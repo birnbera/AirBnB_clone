d8aab44a0adfeb5ad6c1420a3be8043d5dc153c1
jvpupcat <vuong.joann@yahoo.com>
2017-10-01 06:09:41 +0000

    added comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """class BaseModel"""
 
+
 from models import storage
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        """__init__ method & instantiation of class Basemodel"""
+        self.id = str(uuid.uuid4())"""unique id"""
+        self.created_at = datetime.now()"""current datetime"""
+        self.updated_at = self.created_at"""datetime of updated info"""
         for k, v in kwargs.items():
+            """searches through dict for keys"""
             if k in ['created_at', 'updated_at']:
                 v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
             if k == "__class__":
@@ -20,14 +24,17 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
+        """returns __str__ method"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing all k/v of __dict__"""
         d = {}
         d.update(self.__dict__)
         d['created_at'] = d['created_at'].isoformat()
