44735fb2df2734c7810551de5e250a38858a69cd
jvpupcat <vuong.joann@yahoo.com>
2017-10-01 17:25:15 +0000

    removed some comments

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """__init__ method & instantiation of class Basemodel"""
-        self.id = str(uuid.uuid4())"""unique id"""
-        self.created_at = datetime.now()"""current datetime"""
-        self.updated_at = self.created_at"""datetime of updated info"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         for k, v in kwargs.items():
             """searches through dict for keys"""
             if k in ['created_at', 'updated_at']:
