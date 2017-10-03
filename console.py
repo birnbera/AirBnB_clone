281ef5f4f595d0c256468395be22975404aca530
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-03 22:13:16 +0000

    Clean up code, fix pep8

--- console.py
+++ console.py
@@ -6,8 +6,12 @@ import cmd
 import re
 import models
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+
+    def precmd(self, line):
+        return line.strip()
 
     def do_EOF(self, line):
         """End of file - quits console"""
@@ -24,26 +28,26 @@ class HBNBCommand(cmd.Cmd):
         if len(line) == 0:
             print("** class name missing **")
         else:
-            args = line.split()
             try:
-                cls = getattr(models, args[0])
-            except AttributeError:
+                cls = models.class_dict[line]
+            except KeyError:
                 print("** class doesn't exist **")
-                return
-            obj = cls()
-            obj.save()
-            print(obj.id)
+            else:
+                obj = cls()
+                obj.save()
+                print(obj.id)
 
     def do_show(self, line):
         if len(line) == 0:
             print("** class name missing **")
         else:
-            args = line.split()
-            if hasattr(models, args[0]):
-                if len(args) < 2:
+            line = line.split()
+            if line[0] in models.class_dict:
+                try:
+                    obj_id = line[0] + '.' + line[1]
+                except IndexError:
                     print("** instance id missing **")
                 else:
-                    obj_id = '.'.join(args[:2])
                     try:
                         print(models.storage.all()[obj_id])
                     except KeyError:
@@ -55,67 +59,86 @@ class HBNBCommand(cmd.Cmd):
         if len(line) == 0:
             print("** class name missing **")
         else:
-            args = line.split()
-            if hasattr(models, args[0]):
-                if len(args) < 2:
+            line = line.split()
+            if line[0] in models.class_dict:
+                try:
+                    obj_id = line[0] + '.' + line[1]
+                except IndexError:
                     print("** instance id missing **")
                 else:
-                    obj_id = '.'.join(args[:2])
                     try:
                         del models.storage.all()[obj_id]
-                        models.storage.save()
                     except KeyError:
                         print("** no instance found **")
+                    else:
+                        models.storage.save()
             else:
                 print("** class doesn't exist **")
 
     def do_all(self, line):
-        if len(line) > 0:
-            line = line.split()[0]
-        list_found = []
-        if line == "" or hasattr(models, line):
-            for k, v in models.storage.all().items():
-                if line in k:
-                    list_found.append(str(v))
-            print(list_found)
-        else:
+        if len(line) == 0:
+            print([str(v) for v in models.storage.all().values()])
+        elif line not in models.class_dict:
             print("** class doesn't exist **")
+        else:
+            print([str(v) for k, v in models.storage.all().items()
+                   if line in k])
 
     def do_update(self, line):
-        pattern = "[^\s\"\']+|\"[^\"]*\"|\'[^\']*\'"
-        pattern = re.compile(pattern)
         if len(line) == 0:
             print("** class name missing **")
         else:
-            args = re.findall(pattern, line)
-            for i, arg in enumerate(args):
-                args[i] = arg.strip("\"'")
-            if not hasattr(models, args[0]):
+            pattern = "[^\s\"\']+|\"[^\"]*\"|\'[^\']*\'"
+            pattern = re.compile(pattern)
+            line = re.findall(pattern, line)
+            for i in range(len(line)):
+                line[i] = line[i].strip("\"'")
+            if line[0] in models.class_dict:
+                try:
+                    obj_id = line[0] + '.' + line[1]
+                except IndexError:
+                    print("** instance id missing **")
+                else:
+                    try:
+                        obj = models.storage.all()[obj_id]
+                    except KeyError:
+                        print("** no instance found **")
+                    else:
+                        try:
+                            attr = line[2]
+                        except IndexError:
+                            print("** attribute name missing **")
+                        else:
+                            try:
+                                val = line[3]
+                            except IndexError:
+                                print("** value missing **")
+                            else:
+                                setattr(obj, attr, val)
+                                obj.save()
+            else:
                 print("** class doesn't exist **")
-                print(args)
-                return
-            try:
-                obj_id = '.'.join([args[0], args[1]])
-            except IndexError:
-                print("** instance id missing **")
-                return
-            try:
-                obj = models.storage.all()[obj_id]
-            except KeyError:
-                print("** no instance found **")
-                return
-            try:
-                attr = args[2]
-            except IndexError:
-                print("** attribute name missing **")
-                return
-            try:
-                val = args[3]
-            except IndexError:
-                print("** value missing **")
-                return
-            setattr(obj, attr, val)
-            obj.save()
+
+    def do_BaseModel(self, line):
+        pass
+
+    def do_User(self, line):
+        pass
+
+    def do_State(self, line):
+        pass
+
+    def do_City(self, line):
+        pass
+
+    def do_Amenity(self, line):
+        pass
+
+    def do_Place(self, line):
+        pass
+
+    def do_Review(self, line):
+        pass
 
 if __name__ == '__main__':
     """command loop"""
