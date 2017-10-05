d0c76c1e208d96e0b3aad6d74fe1ecb4811ec6cd
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-05 04:19:49 +0000

    Make attributes class level

--- models/user.py
+++ models/user.py
@@ -6,10 +6,7 @@ import models
 
 class User(models.BaseModel):
     """Class to store User info"""
-    def __init__(self, *args, **kwargs):
-        """Initialize new User instances from *args and **kwargs"""
-        self.email = kwargs.pop('email', "")
-        self.password = kwargs.pop('password', "")
-        self.first_name = kwargs.pop('first_name', "")
-        self.last_name = kwargs.pop('last_name', "")
-        super().__init__(*args, **kwargs)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
