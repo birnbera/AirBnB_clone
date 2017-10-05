740a01372980f9f8b6d1c52e24afa1688165d13e
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-05 04:45:41 +0000

    fix problem

--- models/place.py
+++ models/place.py
@@ -6,14 +6,14 @@ import models
 
 class Place(models.BaseModel):
     """Class to store place information"""
-    city_id = 'city_id', "")
-    user_id = 'user_id', "")
-    name = 'name', "")
-    description = 'description', "")
-    number_rooms = 'number_rooms', 0)
-    number_bathrooms = 'number_bathrooms', 0)
-    max_guest = 'max_guest', 0)
-    price_by_night = 'price_by_night', 0)
-    latitude = 'latitude', 0.0)
-    longitude = 'longitude', 0.0)
-    amenity_ids = 'amenity_id', [])
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
