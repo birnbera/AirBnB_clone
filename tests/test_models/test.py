4cf2d345b14aa2d07f5d06d103523039f5c14bcb
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-05 04:10:44 +0000

    Remove test.py from index

--- tests/test_models/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from datetime import datetime
-from unittest import mock
-
-with mock.patch.object(test.datetime, 'now', return_value="woo") as mockdt:
-    a = datetime.now()
-print(a)
