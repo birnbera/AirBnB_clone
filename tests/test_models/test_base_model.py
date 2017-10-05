1e3d76f90bf646dfeae12be96a60bae1ae07ba5f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-05 03:33:45 +0000

    Change datetime tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -84,17 +84,19 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
+        tic = datetime.now()
         inst1 = BaseModel()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst1.created_at <= toc)
         time.sleep(1e-4)
+        tic = datetime.now()
         inst2 = BaseModel()
-        now = datetime.now()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst2.created_at <= toc)
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
         self.assertNotEqual(inst1.updated_at, inst2.updated_at)
-        delta = now - inst1.created_at
-        self.assertGreater(delta.total_seconds(), 0)
-        self.assertAlmostEqual(delta.total_seconds(), 0.0, delta=1e-3)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
